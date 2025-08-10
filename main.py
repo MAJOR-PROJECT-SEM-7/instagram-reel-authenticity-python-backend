a0e551c5ed9514a232311719f56393825527779b
Aniket Raut <rautnaniket@gmail.com>
2025-08-10 18:30:17 +0530

    Add CORS middleware to main application for enhanced API security
    - Integrated CORSMiddleware to allow cross-origin requests, improving the flexibility of the API for frontend applications.
    - Configured middleware settings to permit all origins, methods, and headers, facilitating easier development and testing.

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from app.flow import check_authenticity, check_authenticity_websocket
 import json
 from testing_backend.auth import router as auth_router
 from testing_backend.entires import router as entries_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:3000", "https://yourdomain.com"]
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, PUT, DELETE...
+    allow_headers=["*"],  # Authorization, Content-Type...
+)
+
 app.include_router(router, prefix="/api")
 app.include_router(auth_router, prefix="/api")
 app.include_router(entries_router, prefix="/api")
