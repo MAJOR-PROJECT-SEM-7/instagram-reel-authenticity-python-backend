d7d5c12c9abc377db9213463aca82f6f70d09363
HeetDhanuka08 <heetdhanuka@gmail.com>
2025-10-28 22:46:26 +0530

    Update WebSocket response structure for claim verification
    - Modified the response format for both worthy and not worthy claims in the `websocket_backend` function to include a `success` field, improving clarity in the communication with the client.
    - This change enhances the user experience by providing explicit success indicators for the generated responses.

--- websocketbackend/socket.py
+++ websocketbackend/socket.py
@@ -45,13 +45,13 @@ async def websocket_backend(websocket: WebSocket, url: str):
         if not video_analysis['analysis']['is_worthy']:
             await websocket.send_text(json.dumps({"step": "success", "message": "There is no claim that is worth verifying"}))
             not_worthy_response_data = not_worthy_response(video_analysis['analysis'], video_analysis['analysis']['category'])
-            await websocket.send_text(json.dumps({"step": "completed", "message": "Not worthy response generated" , "response": not_worthy_response_data}))
+            await websocket.send_text(json.dumps({"step": "completed","success": False, "message": "Not worthy response generated" , "response": not_worthy_response_data}))
             await websocket.close()
             return
         else:
             await websocket.send_text(json.dumps({"step": "success", "message": f"There are {len(video_analysis['analysis']['claims'])} claims made in the video"}))
             if_worthy_response_data = await if_worthy_response(video_analysis['analysis']['claims'], websocket=websocket)
-            await websocket.send_text(json.dumps({"step": "completed", "message": "If worthy response generated" , "response": if_worthy_response_data}))
+            await websocket.send_text(json.dumps({"step": "completed", "success": True, "message": "If worthy response generated" , "response": if_worthy_response_data}))
             await websocket.close()
             return
     except Exception as e:
